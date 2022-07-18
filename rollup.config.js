f720a37b6aa55a4013f32afaafe1f0875db7a948
sacru2red <sacru2red@gmail.com>
2022-07-18 19:02:08 +0900

    in esmodule build not use builtin buffer

--- rollup.config.js
+++ rollup.config.js
@@ -24,7 +24,9 @@ export default inputSrc
       preserveEntrySignatures: true,
       plugins: [
         typescript(),
-        nodeResolve(),
+        nodeResolve({
+          preferBuiltins: format === 'cjs'
+        }),
         // https://velog.io/@peterkimzz/rollup-%ED%94%8C%EB%9F%AC%EA%B7%B8%EC%9D%B8
         // CommonJS 로 작성된 모듈들을 ES6 바꾸어서 rollup이 해석할 수 있게 도와줍니다.
         commonjs({
